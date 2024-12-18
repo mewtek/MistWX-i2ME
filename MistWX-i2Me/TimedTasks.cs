38c76d1b33ef8b46f8c60f2b72c503633fe793a6
April P. <mewtek@icloud.com>
2024-12-18 00:55:27 -0800

    Seperate non-essential record generation into its own task

--- MistWX-i2Me/TimedTasks.cs
+++ MistWX-i2Me/TimedTasks.cs
@@ -66,32 +66,47 @@ public class TimedTasks
                 await new CurrentObservationsProduct().Populate(locations);
             List<GenericResponse<DailyForecastResponse>> dfs = await new DailyForecastProduct().Populate(locations);
             List<GenericResponse<HourlyForecastResponse>> hfs = await new HourlyForecastProduct().Populate(locations);
-            List<GenericResponse<AirQualityResponse>> aqs = await new AirQualityProduct().Populate(locations);
-            List<GenericResponse<PollenResponse>> pfs = await new PollenForecastProduct().Populate(locations);
-            List<GenericResponse<HeatingCoolingResponse>> hcs = await new HeatingCoolingProduct().Populate(locations);
-            List<GenericResponse<AchesPainResponse>> acps = await new AchesPainProduct().Populate(locations);
 
             string obsRecord = await new CurrentObsRecord().MakeRecord(obs);
             string dfsRecord = await new DailyForecastRecord().MakeRecord(dfs);
             string hfRecord = await new HourlyForecastRecord().MakeRecord(hfs);
-            string aqsRecord = await new AirQualityRecord().MakeRecord(aqs);
-            string pfsRecord = await new PollenRecord().MakeRecord(pfs);
-            string hcRecord = await new HeatingCoolingRecord().MakeRecord(hcs);
-            string acpsRecord = await new AchesPainRecord().MakeRecord(acps);
             
             sender.SendFile(obsRecord, "storeData(QGROUP=__CurrentObservations__,Feed=CurrentObservations)");
             sender.SendFile(dfsRecord, "storeData(QGROUP=__DailyForecast__,Feed=DailyForecast)");
             sender.SendFile(hfRecord, "storeData(QGROUP=__HourlyForecast__,Feed=HourlyForecast)");
-            sender.SendFile(aqsRecord, "storeData(QGROUP=__AirQuality__,Feed=AirQuality)");
-            sender.SendFile(pfsRecord, "storeData(QGROUP=__PollenForecast__,Feed=PollenForecast)");
-            sender.SendFile(hcRecord, "storeData(QGROUP=__HeatingAndCooling__,Feed=HeatingAndCooling)");
-            sender.SendFile(acpsRecord, "storeData(QGROUP=__AchesAndPains__,Feed=AchesAndPains)");
             
             string nextTimestamp = DateTime.Now.AddHours(1).ToString("h:mm tt");
             
             Log.Info($"Next record generation will be at {nextTimestamp}");
             
             await Task.Delay(3600 * 1000);
+        }
+    }
+
+    public static async Task BiHourlyCollection(string[] locations, UdpSender sender)
+    {
+        while (true)
+        {
+            Log.Info("Running bi-hourly record collection");
+            List<GenericResponse<AirQualityResponse>> aqs = await new AirQualityProduct().Populate(locations);
+            List<GenericResponse<PollenResponse>> pfs = await new PollenForecastProduct().Populate(locations);
+            List<GenericResponse<HeatingCoolingResponse>> hcs = await new HeatingCoolingProduct().Populate(locations);
+            List<GenericResponse<AchesPainResponse>> acps = await new AchesPainProduct().Populate(locations);
+            List<GenericResponse<BreathingResponse>> brs = await new BreathingProduct().Populate(locations);
+            
+            string aqsRecord = await new AirQualityRecord().MakeRecord(aqs);
+            string pfsRecord = await new PollenRecord().MakeRecord(pfs);
+            string hcRecord = await new HeatingCoolingRecord().MakeRecord(hcs);
+            string acpsRecord = await new AchesPainRecord().MakeRecord(acps);
+            string brsRecord = await new BreathingRecord().MakeRecord(brs);
+            
+            sender.SendFile(aqsRecord, "storeData(QGROUP=__AirQuality__,Feed=AirQuality)");
+            sender.SendFile(pfsRecord, "storeData(QGROUP=__PollenForecast__,Feed=PollenForecast)");
+            sender.SendFile(hcRecord, "storeData(QGROUP=__HeatingAndCooling__,Feed=HeatingAndCooling)");
+            sender.SendFile(acpsRecord, "storeData(QGROUP=__AchesAndPains__,Feed=AchesAndPains)");
+            sender.SendFile(brsRecord, "storeData(QGROUP=__Breathing__,Feed=Breathing)");
+
+            await Task.Delay(7200 * 1000);
         }
     }
 }
