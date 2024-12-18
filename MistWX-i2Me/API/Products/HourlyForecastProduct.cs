a1dbc8b03b52b899863ebf6cb94acc323b0e35ae
April P. <mewtek@icloud.com>
2024-12-17 17:28:05 -0800

    Add Hourly Forecast product

---
+++ MistWX-i2Me/API/Products/HourlyForecastProduct.cs
@@ -0,0 +1,18 @@
+using MistWX_i2Me.Schema.ibm;
+
+namespace MistWX_i2Me.API.Products;
+
+public class HourlyForecastProduct : Base
+{
+    public HourlyForecastProduct()
+    {
+        RecordName = "HourlyForecast";
+        DataUrl =
+            "https://api.weather.com/v1/location/{zip}:4:US/forecast/hourly/360hour.xml?language=en-US&units=e&apiKey={apiKey}";
+    }
+
+    public async Task<List<GenericResponse<HourlyForecastResponse>>> Populate(string[] locations)
+    {
+        return await GetData<HourlyForecastResponse>(locations);
+    }
+}
