7f667f9bb37c110aad45e9b131be63b2ba2be532
April P. <mewtek@icloud.com>
2024-12-22 18:11:21 -0800

    Add global alert cache

--- MistWX-i2Me/Globals.cs
+++ MistWX-i2Me/Globals.cs
@@ -4,5 +4,12 @@ namespace MistWX_i2Me;
 
 public static class Globals
 {
+    /// <summary>
+    /// Memory cache for storing LFRecord objects
+    /// </summary>
     public static IMemoryCache LocationCache = new MemoryCache(new MemoryCacheOptions());
+    /// <summary>
+    /// Memory cache for storing AlertHeadline IDs and their UTC expiration times
+    /// </summary>
+    public static IMemoryCache AlertsCache = new MemoryCache(new MemoryCacheOptions());
 }
