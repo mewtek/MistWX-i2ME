c9ffc637a576037206fd256d45c8b5d2ef1486d7
April P. <mewtek@icloud.com>
2024-12-22 18:11:38 -0800

    Cache alerts during generation

--- MistWX-i2Me/API/Products/AlertDetailsProduct.cs
+++ MistWX-i2Me/API/Products/AlertDetailsProduct.cs
@@ -1,4 +1,5 @@
 using System.Text.Json;
+using Microsoft.Extensions.Caching.Memory;
 using MistWX_i2Me.Schema.ibm;
 
 namespace MistWX_i2Me.API.Products;
@@ -11,11 +12,11 @@ public class AlertDetailsProduct : Base
         DataUrl = "N/A";    // Handled differently due to the endpoints used
     }
 
-    public async Task<List<GenericResponse<AlertDetailResponse>>> Populate(
-        List<GenericResponse<HeadlineResponse>> headlines)
+    public async Task<List<GenericResponse<AlertDetailResponse>>> Populate(List<GenericResponse<HeadlineResponse>> headlines)
     {
+        IMemoryCache alertsCache = Globals.AlertsCache;
         List<GenericResponse<AlertDetailResponse>> results = new();
-
+        
         foreach (var headline in headlines)
         {
             foreach (Alert alert in headline.ParsedData.alerts)
@@ -23,8 +24,13 @@ public class AlertDetailsProduct : Base
                 string url =
                     $"https://api.weather.com/v3/alerts/detail?alertId={alert.detailKey}&format=json&language=en-US&apiKey={Config.config.TwcApiKey}";
                 string? res = await DownloadRecord(url);
-
+                
                 if (res == null)
+                {
+                    continue;
+                }
+                
+                if (alertsCache.TryGetValue(alert.detailKey, out int expireTime))
                 {
                     continue;
                 }
@@ -33,15 +39,12 @@ public class AlertDetailsProduct : Base
                 {
                     AlertDetailResponse? response = await JsonSerializer.DeserializeAsync<AlertDetailResponse?>(stream);
                     results.Add(new GenericResponse<AlertDetailResponse>(headline.Location, res, response));
-                    Log.Debug("NEW ALERT\n" +
-                              $"ETN: {alert.eventTrackingNumber}\n" +
-                              $"Desc: {alert.eventDescription}\n" +
-                              $"Expiration Time Local: {alert.expireTimeLocal.ToString()}\n" +
-                              $"Expiration Time UTC: {alert.expireTimeUTC}");
+                    alertsCache.Set(alert.detailKey, alert.expireTimeUTC);
+                    Log.Debug($"Alert ID {alert.detailKey} cached, expires @ {alert.expireTimeUTC} UTC.");
                 }
             }
         }
-
+        
         return results;
     }
 }
